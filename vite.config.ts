5e149dc6ffafb091dbbf13a7f36b420dc052e2db
thanh711 <thanh711@gmail.com>
2026-08-21 22:33:25 +0700

    add icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/start-with-hika/" // phải đúng tên repo
+  base: "/start-with-hika/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'icon-192-purple-a.png',
+        'icon-512-purple-a.png',
+        'favicon.svg'
+      ],
+      manifest: false, // vì bạn đã tự tạo manifest.json trong public/
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ]
 })
